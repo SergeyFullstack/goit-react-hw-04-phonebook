b9b392f536fd34ecff8f3caee77e29d3c913d736
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-06-17 12:49:40 +0300

    Create hw-04 task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './ContactList/Filter/Filter';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const KEY_STORAGE_CONTACTS = 'contacts';
+
+export function App() {
+  const [contacts, setContacts] = useState(() => getStorageContacts() || []);
+  const [filter, setFilter] = useState('');
+
+  function getStorageContacts() {
+    return JSON.parse(localStorage.getItem(KEY_STORAGE_CONTACTS));
+  }
+
+  useEffect(() => {
+    localStorage.setItem(KEY_STORAGE_CONTACTS, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onSubmitForm = newContact => {
+    setContacts([...contacts, ...[newContact]]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const removeContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  const filteredContacts = () => {
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmitForm={onSubmitForm} contacts={contacts} />
+      {contacts.length > 0 && (
+        <>
+          <h2>Contacts</h2>
+          <Filter filter={filter} changeFilter={changeFilter} />
+          <ContactList
+            contacts={filteredContacts()}
+            removeContact={removeContact}
+          />
+          <ToastContainer />
+        </>
+      )}
+    </section>
   );
-};
+}
